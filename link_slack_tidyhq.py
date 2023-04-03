098bc369a2d13786803880245fac1a62e07b2908
Fletcher <me@fboyd.me>
2023-04-03 14:34:20 +0800

    Add manually specified IDs, add logging

--- link_slack_tidyhq.py
+++ link_slack_tidyhq.py
@@ -8,6 +8,8 @@ from datetime import datetime
 with open("config.json","r") as f:
     config: dict = json.load(f)
 
+logging.basicConfig(level=logging.INFO)
+
 # Initiate Slack client
 app = App(token=config["slack"]["bot_token"])
 
@@ -49,15 +51,23 @@ def get_tidyhq_memberships():
             active.append(membership["contact_id"])
     return active
 
+logging.info("Getting Slack users...")
 slack_users = get_slack()
+logging.debug(f'Got {len(slack_users)} Slack users')
+
+logging.info("Getting TidyHQ users...")
 tidyhq_users = get_tidyhq()
+logging.debug(f'Got {len(tidyhq_users)} TidyHQ users')
+
+logging.info("Getting TidyHQ active users...")
 tidyhq_active_users = get_tidyhq_memberships()
+logging.debug(f'Got {len(tidyhq_active_users)} TidyHQ active users')
 
 # Compare the two lists and link any users that are in both
 for email in tidyhq_users:
+    t = tidyhq_users[email]
     if email in slack_users:
         print("\n")
-        t = tidyhq_users[email]
         s = slack_users[email]
         # Link the user
         print(f"Match:\nTidyHQ: {t['first_name']} {t['last_name']} ({t['id']})\nSlack: {s['name']} ({s['id']})")
@@ -72,8 +82,6 @@ for email in tidyhq_users:
                     print(r.text)
                 else:
                     print("Linked")
-                    # Remove the user from the TidyHQ list of people already linked
-                    del tidyhq_users[email]
 
             except requests.exceptions.RequestException as e:
                 logging.error(f"Could not link {tidyhq_users[email]['name']}")
@@ -82,3 +90,18 @@ for email in tidyhq_users:
     else:
         if tidyhq_users[email]["id"] in tidyhq_active_users:
             print(f"Could not find {tidyhq_users[email]['first_name']} {tidyhq_users[email]['last_name']} in Slack but they are an active member")
+            slack_id = input("Enter Slack ID [or leave blank to skip]: ")
+            if slack_id:
+                try:
+                    r = requests.put(f"https://api.tidyhq.com/v1/contacts/{t['id']}",
+                                    params={"access_token":config["tidyhq"]["token"]},
+                                    json={"custom_fields":{config["tidyhq"]["ids"]["slack"]:slack_id}})
+                    if r.status_code != 200:
+                        print(f"Could not link")
+                        print(r.text)
+                    else:
+                        print("Linked")
+                except requests.exceptions.RequestException as e:
+                    logging.error(f"Could not link {tidyhq_users[email]['name']}")
+            else:
+                print("Skipping")

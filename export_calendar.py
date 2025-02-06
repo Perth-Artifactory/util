d978ad5ada1bece0906d521ff9f9ee440ae10a77
Fletcher <me@fboyd.me>
2025-02-06 14:49:36 +0800

    Formatting, fix type hints, remove unused error capture

--- export_calendar.py
+++ export_calendar.py
@@ -11,31 +11,42 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-with open("config.json","r") as f:
+with open("config.json", "r") as f:
     config: dict = json.load(f)
 
-def gcal() -> None:
-   
-    creds = Credentials.from_authorized_user_file('token.json', ['https://www.googleapis.com/auth/calendar.readonly'])
+
+def gcal() -> str:
+
+    creds = Credentials.from_authorized_user_file(
+        "token.json", ["https://www.googleapis.com/auth/calendar.readonly"]
+    )
     try:
-        service = build('calendar', 'v3', credentials=creds)
+        service = build("calendar", "v3", credentials=creds)
 
         # Call the Calendar API
-        now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        events_result = service.events().list(calendarId=config["google"]["calendar_id"], timeMin=now,
-                                              maxResults=30, singleEvents=True,
-                                              orderBy='startTime').execute()
-        events = events_result.get('items', [])
+        now = datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+        events_result = (
+            service.events()  # type: ignore
+            .list(
+                calendarId=config["google"]["calendar_id"],
+                timeMin=now,
+                maxResults=30,
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
+        )
+        events = events_result.get("items", [])
 
         if not events:
             sys.exit(1)
-            return
+            return ""
 
         # Construct more useful event dict
-        formated_events = []
+        formatted_events = []
         for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            end = event['end'].get('dateTime', event['end'].get('date'))
+            start = event["start"].get("dateTime", event["start"].get("date"))
+            end = event["end"].get("dateTime", event["end"].get("date"))
             if "description" not in event:
                 event["description"] = ""
             # Add fake hours to all day events
@@ -43,42 +54,53 @@ def gcal() -> None:
                 start = start + "T00:00:00+08:00"
             if len(end) == 10:
                 end = end + "T00:00:00+08:00"
-            
-            formated_events.append({
-                "start": start,
-                "end": end,
-                "summary": event['summary'],
-                "description": event['description']
-            })
-        return json.dumps(formated_events, indent=4)
 
-    except HttpError as error:
+            formatted_events.append(
+                {
+                    "start": start,
+                    "end": end,
+                    "summary": event["summary"],
+                    "description": event["description"],
+                }
+            )
+        return json.dumps(formatted_events, indent=4)
+
+    except HttpError:
         sys.exit()
 
-# Retrives the next 30 events from tidyhq, formats them the same as gcal and returns them as a json string
-def tidyhq() -> None:
-    try:
-        r = requests.get("https://api.tidyhq.com/v1/events",params={"access_token":config["tidyhq"]["token"],
-                                                                    "limit":30,
-                                                                    "start_at":datetime.utcnow().isoformat() + 'Z',
-                                                                    "public":True})
-        events = r.json()
-    except requests.exceptions.RequestException as e:
-        return False
-    formated_events = []
-    for event in events:
-        formated_events.append({
-            "start": event['start_at'],
-            "end": event['end_at'],
-            "summary": event['name'],
-            "description": event['body'],
-            "location": event['location'],
-            "url": event['public_url'],
-            "id": event['id']
-        })
-    return json.dumps(formated_events, indent=4)
 
-if __name__ == '__main__':
+# Retrieves the next 30 events from tidyhq, formats them the same as gcal and returns them as a json string
+def tidyhq() -> str:
+    try:
+        r = requests.get(
+            "https://api.tidyhq.com/v1/events",
+            params={
+                "access_token": config["tidyhq"]["token"],
+                "limit": 30,
+                "start_at": datetime.utcnow().isoformat() + "Z",
+                "public": True,
+            },
+        )
+        events = r.json()
+    except requests.exceptions.RequestException:
+        return ""
+    formatted_events = []
+    for event in events:
+        formatted_events.append(
+            {
+                "start": event["start_at"],
+                "end": event["end_at"],
+                "summary": event["name"],
+                "description": event["body"],
+                "location": event["location"],
+                "url": event["public_url"],
+                "id": event["id"],
+            }
+        )
+    return json.dumps(formatted_events, indent=4)
+
+
+if __name__ == "__main__":
     if len(sys.argv) > 1:
         if sys.argv[1] == "gcal":
             print(gcal())

81b1129eb758644d9303c06c658a0922ae5bde8a
Fletcher <me@fboyd.me>
2025-02-06 14:36:10 +0800

    Allow token name to be easily changed, add spelling

--- auth.google.py
+++ auth.google.py
@@ -8,52 +8,67 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-with open("config.json","r") as f:
+with open("config.json", "r") as f:
     config: dict = json.load(f)
+
 
 def main() -> None:
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
+    token = "token.json"
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', ['https://www.googleapis.com/auth/calendar.readonly'])
+    if os.path.exists(token):
+        creds = Credentials.from_authorized_user_file(
+            token, ["https://www.googleapis.com/auth/calendar.readonly"]
+        )
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'google.secret.json', ['https://www.googleapis.com/auth/calendar.readonly'])
+                "google.secret.json",
+                ["https://www.googleapis.com/auth/calendar.readonly"],
+            )
             creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
+        with open(token, "w") as token:
             token.write(creds.to_json())
 
     try:
-        service = build('calendar', 'v3', credentials=creds)
+        service = build("calendar", "v3", credentials=creds)
 
         # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print('Getting the next event as a test')
-        events_result = service.events().list(calendarId=config["google"]["calendar_id"], timeMin=now,
-                                              maxResults=1, singleEvents=True,
-                                              orderBy='startTime').execute()
-        events = events_result.get('items', [])
+        now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+        print("Getting the next event as a test")
+        events_result = (
+            service.events()  # type: ignore
+            .list(
+                calendarId=config["google"]["calendar_id"],
+                timeMin=now,
+                maxResults=1,
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
+        )
+        events = events_result.get("items", [])
 
         if not events:
-            print('No upcoming events found.')
+            print("No upcoming events found.")
             return
 
         # Print the next event
         for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
+            start = event["start"].get("dateTime", event["start"].get("date"))
+            print(start, event["summary"])
 
     except HttpError as error:
-        print('An error occurred: %s' % error)
+        print("An error occurred: %s" % error)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()

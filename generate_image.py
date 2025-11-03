d97510884acca14d15ad46e9d27cd23b9479be9d
Fletcher <me@fboyd.me>
2025-11-03 22:19:34 +0800

    Format with Black

--- generate_image.py
+++ generate_image.py
@@ -3,11 +3,11 @@ import json
 
 from PIL import Image, ImageDraw
 
-with open('message_data.json', 'r') as f:
+with open("message_data.json", "r") as f:
     message_data = json.load(f)
 
 try:
-    ts = float(message_data['ts'])
+    ts = float(message_data["ts"])
 except ValueError:
     raise ValueError("Invalid timestamp")
 
@@ -29,29 +29,47 @@ elif hours < 1:
 else:
     time_string = past_time.strftime("%H:%M")
 
-name_string = message_data['attachments'][0]['fields'][0]['value']
+name_string = message_data["attachments"][0]["fields"][0]["value"]
 
 height = 180
 width = 1920
-im = Image.new('RGB', (width, height), color = (0, 0, 0))
+im = Image.new("RGB", (width, height), color=(0, 0, 0))
 
 draw = ImageDraw.Draw(im)
 
 boarder = 20
 bar_width = 10
-draw.rounded_rectangle((boarder, boarder, boarder+bar_width, height-boarder), fill=(255, 0, 0), outline=(255, 0, 0), radius=10)
+draw.rounded_rectangle(
+    (boarder, boarder, boarder + bar_width, height - boarder),
+    fill=(255, 0, 0),
+    outline=(255, 0, 0),
+    radius=10,
+)
 
-time_str_left, _, _, _ = draw.textbbox((width-boarder, height/2), time_string, font_size=140, anchor='rm')
-max_name_width = time_str_left - boarder*2 - bar_width
+time_str_left, _, _, _ = draw.textbbox(
+    (width - boarder, height / 2), time_string, font_size=140, anchor="rm"
+)
+max_name_width = time_str_left - boarder * 2 - bar_width
 
 while draw.textlength(name_string, font_size=140) > max_name_width:
     name_string = name_string[:-4] + "..."
 
-draw.text((boarder*2+bar_width, height/2), name_string, fill=(255, 255, 255), font_size=140, anchor='lm')
+draw.text(
+    (boarder * 2 + bar_width, height / 2),
+    name_string,
+    fill=(255, 255, 255),
+    font_size=140,
+    anchor="lm",
+)
 # draw.rectangle(draw.textbbox((boarder*2+bar_width, height/2), name_string, font_size=140, anchor='lm'), outline=(255, 0, 0), width=1)
 
-draw.text((width-boarder, height/2), time_string, fill=(200, 200, 200), font_size=140, anchor='rm')
+draw.text(
+    (width - boarder, height / 2),
+    time_string,
+    fill=(200, 200, 200),
+    font_size=140,
+    anchor="rm",
+)
 # draw.rectangle(draw.textbbox((width-boarder, height/2), time_string, font_size=140, anchor='rm'), outline=(255, 0, 0), width=1)
 
-im.save('door_message.jpg')
-
+im.save("door_message.jpg")
